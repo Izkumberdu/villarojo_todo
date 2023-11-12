7b849ad582c2cae36021f0b7144e11fb79a83d0e
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2023-11-12 13:22:57 +0800

    added flutter slidable

--- lib/Functions/todo_tile.dart
+++ lib/Functions/todo_tile.dart
@@ -15,25 +15,27 @@ class ToDoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: 25.0, right: 25, top: 25),
-      child: Container(
-        padding: EdgeInsets.all(24),
-        decoration: BoxDecoration(
-            color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
-        child: Row(
-          children: [
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              activeColor: Colors.black,
-            ),
-            Text(
-              taskName,
-              style: TextStyle(
-                  decoration: taskCompleted
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none),
-            ),
-          ],
+      child: slidable(
+        child: Container(
+          padding: EdgeInsets.all(24),
+          decoration: BoxDecoration(
+              color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black,
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                    decoration: taskCompleted
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none),
+              ),
+            ],
+          ),
         ),
       ),
     );
